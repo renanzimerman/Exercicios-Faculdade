7aecd6b647c933561f04909c8d73780a22c18169
Renan Zimerman Leite <renanzimermanleite@gmail.com>
2020-04-23 20:29:56 -0300

    ex2

--- Exercicios/ex1.7.1/171.c
+++ Exercicios/ex1.7.1/171.c
@@ -1,3 +1,9 @@
+/*-------------------------------------------------
+Descrição: Esse programa imprime na tela "Olá"
+Autor: Renan Zimerman Leite
+Data: 23/04/2020
+---------------------------------------------------*/
+
 int main()
 {
     printf("Olá!");
--- Exercicios/ex1.7.2/172.c
+++ Exercicios/ex1.7.2/172.c
@@ -1,3 +1,9 @@
+/*---------------------------------------------------
+Descrição: Esse progrma imprime na tela o meu nome
+Autor: Renan Zimerman Leite
+Data: 23/04/2020
+-----------------------------------------------------*/
+
 int main()
 {
     printf("Renan Zimerman Leite");
